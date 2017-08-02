955c26e6ed2bc192b4363284fc894a30c5636b78
Tsung Wu <tsung.bz@gmail.com>
2017-08-02 14:20:21 +0800

    support type constructor

--- library/src/main/java/bz/tsung/android/objectify/ObjectPreferenceLoader.java
+++ library/src/main/java/bz/tsung/android/objectify/ObjectPreferenceLoader.java
@@ -15,6 +15,17 @@ public class ObjectPreferenceLoader {
     Context context;
     Gson gson;
 
+    public <T> ObjectPreferenceLoader(Context context, final String key, Type clazz) {
+        this(context, key, clazz, new Gson());
+    }
+
+    public <T> ObjectPreferenceLoader(Context context, final String key, Type clazz, Gson gson) {
+        this.context = context;
+        this.clazz = clazz;
+        stringPreferenceLoader = new StringPreferenceLoader(context, key);
+        this.gson = gson;
+    }
+
     public <T> ObjectPreferenceLoader(Context context, final String key, Class<T> clazz) {
         this(context, key, clazz, new Gson());
     }
@@ -36,7 +47,7 @@ public class ObjectPreferenceLoader {
     }
 
     public <T> void save(T value) {
-        if (((Class)clazz).isInstance(value)) {
+        if ((clazz instanceof Class && ((Class)clazz).isInstance(value)) || clazz instanceof Type) {
             stringPreferenceLoader.save(gson.toJson(value));
         } else {
             throw new ShouldSaveSameTypeValueException();
